3056a7bf10855990cdfeb4d8c7a39ab3704061bd
adana <adanasiadka@gmail.com>
2023-06-08 17:38:51 +0200

    Przypisanie wartosci inflacji, przypisanie zmiennych do formy wyliczenia wartosci zadluzenia w kazdym miesiacu

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
-# This is a sample Python script.
+inflacja1 = 1.592824484
+inflacja2 = -0.453509101
+inflacja3 = 2.324671717
+inflacja4 = 1.261254407
+inflacja5 = 1.782526286
+inflacja6 = 2.329384541
+inflacja7 = 1.502229842
+inflacja8 = 1.782526286
+inflacja9 = 2.328848994
+inflacja10 = 0.616921348
+inflacja11 = 2.352295886
+inflacja12 = 0.337779545
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+oprocentowanie_kredytu = float(input("Podaj oprocentowanie kredytu: "))
 
+kwota_poczatkowa_kredytu = float(input("Podaj kwote poczatkowa kredytu: "))
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+stala_wartosc_raty = float(input("Podaj stala wartosc raty: "))
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-print('Hello World'
-      '')
+kwota_po_pierwszej_racie = (1+ ((inflacja1+oprocentowanie_kredytu)/1200)) * kwota_poczatkowa_kredytu - stala_wartosc_raty
+print(kwota_po_pierwszej_racie)
